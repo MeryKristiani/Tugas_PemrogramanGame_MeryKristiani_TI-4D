9c2f2fbe3d2350a1649612439518c60b928a89e7
MeryKristiani <merykristiani2@gmail.com>
2020-09-07 12:42:26 +0700

    Pertemuan 2

---
+++ Pertemuan 2/VR Pertemuan 2/Assets/Capsule.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capsule : MonoBehaviour
+{
+    public void Magenta () 
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+    public void Abu () 
+    {
+        GetComponent<Renderer>().material.color = Color.grey;
+    }
+    public void biruTua () 
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+}
---
+++ Pertemuan 2/VR Pertemuan 2/Assets/Cylinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    public void Kuning () 
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    public void Hijau () 
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void Putih () 
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
